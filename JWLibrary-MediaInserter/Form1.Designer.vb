8309248103148809255ce67867734fc46c919230
Dario Ragusa <46404000+darioragusa@users.noreply.github.com>
2022-08-04 16:52:28 +0200

    Resize window #12

--- JWLibrary-MediaInserter/Form1.Designer.vb
+++ JWLibrary-MediaInserter/Form1.Designer.vb
@@ -41,65 +41,69 @@ Partial Class Form1
         '
         'ComboBoxPub
         '
+        Me.ComboBoxPub.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ComboBoxPub.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBoxPub.FormattingEnabled = True
         Me.ComboBoxPub.IntegralHeight = False
-        Me.ComboBoxPub.Location = New System.Drawing.Point(11, 47)
-        Me.ComboBoxPub.Margin = New System.Windows.Forms.Padding(6)
+        Me.ComboBoxPub.Location = New System.Drawing.Point(6, 22)
         Me.ComboBoxPub.Name = "ComboBoxPub"
-        Me.ComboBoxPub.Size = New System.Drawing.Size(717, 40)
+        Me.ComboBoxPub.Size = New System.Drawing.Size(388, 23)
         Me.ComboBoxPub.TabIndex = 1
         '
         'ComboBoxDocs
         '
+        Me.ComboBoxDocs.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ComboBoxDocs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBoxDocs.FormattingEnabled = True
-        Me.ComboBoxDocs.Location = New System.Drawing.Point(11, 128)
-        Me.ComboBoxDocs.Margin = New System.Windows.Forms.Padding(6)
+        Me.ComboBoxDocs.Location = New System.Drawing.Point(6, 60)
         Me.ComboBoxDocs.Name = "ComboBoxDocs"
-        Me.ComboBoxDocs.Size = New System.Drawing.Size(554, 40)
+        Me.ComboBoxDocs.Size = New System.Drawing.Size(300, 23)
         Me.ComboBoxDocs.TabIndex = 2
         '
         'ButtonSelect
         '
+        Me.ButtonSelect.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonSelect.BackColor = System.Drawing.Color.Transparent
         Me.ButtonSelect.FlatAppearance.BorderSize = 0
         Me.ButtonSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonSelect.ForeColor = System.Drawing.Color.Blue
-        Me.ButtonSelect.Location = New System.Drawing.Point(11, 34)
-        Me.ButtonSelect.Margin = New System.Windows.Forms.Padding(6)
+        Me.ButtonSelect.Location = New System.Drawing.Point(6, 16)
         Me.ButtonSelect.Name = "ButtonSelect"
-        Me.ButtonSelect.Size = New System.Drawing.Size(511, 49)
+        Me.ButtonSelect.Size = New System.Drawing.Size(275, 23)
         Me.ButtonSelect.TabIndex = 4
         Me.ButtonSelect.Text = "Select medias to insert"
         Me.ButtonSelect.UseVisualStyleBackColor = False
         '
         'ButtonDelete
         '
+        Me.ButtonDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonDelete.BackColor = System.Drawing.Color.Transparent
         Me.ButtonDelete.FlatAppearance.BorderSize = 0
         Me.ButtonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonDelete.ForeColor = System.Drawing.Color.Red
-        Me.ButtonDelete.Location = New System.Drawing.Point(531, 34)
-        Me.ButtonDelete.Margin = New System.Windows.Forms.Padding(6)
+        Me.ButtonDelete.Location = New System.Drawing.Point(286, 16)
         Me.ButtonDelete.Name = "ButtonDelete"
-        Me.ButtonDelete.Size = New System.Drawing.Size(199, 49)
+        Me.ButtonDelete.Size = New System.Drawing.Size(107, 23)
         Me.ButtonDelete.TabIndex = 5
         Me.ButtonDelete.Text = "Delete"
         Me.ButtonDelete.UseVisualStyleBackColor = False
         '
         'GroupBoxMedia
         '
+        Me.GroupBoxMedia.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBoxMedia.BackColor = System.Drawing.Color.White
         Me.GroupBoxMedia.Controls.Add(Me.ListBoxMedia)
         Me.GroupBoxMedia.Controls.Add(Me.ButtonInsert)
         Me.GroupBoxMedia.Controls.Add(Me.ButtonSelect)
         Me.GroupBoxMedia.Controls.Add(Me.ButtonDelete)
-        Me.GroupBoxMedia.Location = New System.Drawing.Point(22, 243)
-        Me.GroupBoxMedia.Margin = New System.Windows.Forms.Padding(6)
+        Me.GroupBoxMedia.Location = New System.Drawing.Point(12, 114)
         Me.GroupBoxMedia.Name = "GroupBoxMedia"
-        Me.GroupBoxMedia.Padding = New System.Windows.Forms.Padding(6)
-        Me.GroupBoxMedia.Size = New System.Drawing.Size(743, 525)
+        Me.GroupBoxMedia.Size = New System.Drawing.Size(400, 251)
         Me.GroupBoxMedia.TabIndex = 3
         Me.GroupBoxMedia.TabStop = False
         Me.GroupBoxMedia.Text = "Media"
@@ -107,80 +111,87 @@ Partial Class Form1
         'ListBoxMedia
         '
         Me.ListBoxMedia.AllowDrop = True
+        Me.ListBoxMedia.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ListBoxMedia.BackColor = System.Drawing.SystemColors.Window
         Me.ListBoxMedia.FormattingEnabled = True
-        Me.ListBoxMedia.ItemHeight = 32
-        Me.ListBoxMedia.Location = New System.Drawing.Point(11, 98)
-        Me.ListBoxMedia.Margin = New System.Windows.Forms.Padding(6)
+        Me.ListBoxMedia.ItemHeight = 15
+        Me.ListBoxMedia.Location = New System.Drawing.Point(6, 46)
         Me.ListBoxMedia.Name = "ListBoxMedia"
-        Me.ListBoxMedia.Size = New System.Drawing.Size(717, 356)
+        Me.ListBoxMedia.Size = New System.Drawing.Size(388, 169)
         Me.ListBoxMedia.TabIndex = 6
         '
         'ButtonInsert
         '
+        Me.ButtonInsert.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonInsert.BackColor = System.Drawing.Color.Transparent
         Me.ButtonInsert.FlatAppearance.BorderSize = 0
         Me.ButtonInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonInsert.ForeColor = System.Drawing.Color.Blue
-        Me.ButtonInsert.Location = New System.Drawing.Point(11, 467)
-        Me.ButtonInsert.Margin = New System.Windows.Forms.Padding(6)
+        Me.ButtonInsert.Location = New System.Drawing.Point(6, 221)
         Me.ButtonInsert.Name = "ButtonInsert"
-        Me.ButtonInsert.Size = New System.Drawing.Size(717, 49)
+        Me.ButtonInsert.Size = New System.Drawing.Size(388, 23)
         Me.ButtonInsert.TabIndex = 7
         Me.ButtonInsert.Text = "Insert"
         Me.ButtonInsert.UseVisualStyleBackColor = False
         '
         'GroupBoxPub
         '
+        Me.GroupBoxPub.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBoxPub.Controls.Add(Me.ButtonRestoreBackup)
         Me.GroupBoxPub.Controls.Add(Me.ComboBoxPub)
         Me.GroupBoxPub.Controls.Add(Me.ComboBoxDocs)
-        Me.GroupBoxPub.Location = New System.Drawing.Point(22, 26)
-        Me.GroupBoxPub.Margin = New System.Windows.Forms.Padding(6)
+        Me.GroupBoxPub.Location = New System.Drawing.Point(12, 12)
         Me.GroupBoxPub.Name = "GroupBoxPub"
-        Me.GroupBoxPub.Padding = New System.Windows.Forms.Padding(6)
-        Me.GroupBoxPub.Size = New System.Drawing.Size(743, 205)
+        Me.GroupBoxPub.Size = New System.Drawing.Size(400, 96)
         Me.GroupBoxPub.TabIndex = 0
         Me.GroupBoxPub.TabStop = False
         Me.GroupBoxPub.Text = "Publication"
         '
         'ButtonRestoreBackup
         '
+        Me.ButtonRestoreBackup.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonRestoreBackup.BackColor = System.Drawing.Color.Transparent
         Me.ButtonRestoreBackup.Enabled = False
         Me.ButtonRestoreBackup.FlatAppearance.BorderSize = 0
         Me.ButtonRestoreBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonRestoreBackup.Font = New System.Drawing.Font("Segoe UI", 7.6!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.ButtonRestoreBackup.ForeColor = System.Drawing.Color.Red
-        Me.ButtonRestoreBackup.Location = New System.Drawing.Point(572, 109)
-        Me.ButtonRestoreBackup.Margin = New System.Windows.Forms.Padding(6)
+        Me.ButtonRestoreBackup.Location = New System.Drawing.Point(308, 51)
         Me.ButtonRestoreBackup.Name = "ButtonRestoreBackup"
-        Me.ButtonRestoreBackup.Size = New System.Drawing.Size(167, 83)
+        Me.ButtonRestoreBackup.Size = New System.Drawing.Size(90, 39)
         Me.ButtonRestoreBackup.TabIndex = 6
         Me.ButtonRestoreBackup.Text = "Restore backup"
         Me.ButtonRestoreBackup.UseVisualStyleBackColor = False
         '
         'GroupBoxAdvanced
         '
+        Me.GroupBoxAdvanced.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBoxAdvanced.Controls.Add(Me.ButtonJWPUB)
         Me.GroupBoxAdvanced.Controls.Add(Me.CheckBoxShowAll)
-        Me.GroupBoxAdvanced.Location = New System.Drawing.Point(22, 777)
+        Me.GroupBoxAdvanced.Location = New System.Drawing.Point(12, 369)
+        Me.GroupBoxAdvanced.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.GroupBoxAdvanced.Name = "GroupBoxAdvanced"
-        Me.GroupBoxAdvanced.Size = New System.Drawing.Size(743, 91)
+        Me.GroupBoxAdvanced.Padding = New System.Windows.Forms.Padding(2, 1, 2, 1)
+        Me.GroupBoxAdvanced.Size = New System.Drawing.Size(400, 43)
         Me.GroupBoxAdvanced.TabIndex = 4
         Me.GroupBoxAdvanced.TabStop = False
         Me.GroupBoxAdvanced.Text = "Advanced"
         '
         'ButtonJWPUB
         '
+        Me.ButtonJWPUB.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonJWPUB.BackColor = System.Drawing.Color.Transparent
         Me.ButtonJWPUB.FlatAppearance.BorderSize = 0
         Me.ButtonJWPUB.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonJWPUB.ForeColor = System.Drawing.Color.Blue
-        Me.ButtonJWPUB.Location = New System.Drawing.Point(460, 31)
-        Me.ButtonJWPUB.Margin = New System.Windows.Forms.Padding(6)
+        Me.ButtonJWPUB.Location = New System.Drawing.Point(248, 15)
         Me.ButtonJWPUB.Name = "ButtonJWPUB"
-        Me.ButtonJWPUB.Size = New System.Drawing.Size(268, 49)
+        Me.ButtonJWPUB.Size = New System.Drawing.Size(146, 23)
         Me.ButtonJWPUB.TabIndex = 5
         Me.ButtonJWPUB.Text = "Export JWPUB"
         Me.ButtonJWPUB.UseVisualStyleBackColor = False
@@ -188,9 +199,10 @@ Partial Class Form1
         'CheckBoxShowAll
         '
         Me.CheckBoxShowAll.AutoSize = True
-        Me.CheckBoxShowAll.Location = New System.Drawing.Point(11, 38)
+        Me.CheckBoxShowAll.Location = New System.Drawing.Point(6, 18)
+        Me.CheckBoxShowAll.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.CheckBoxShowAll.Name = "CheckBoxShowAll"
-        Me.CheckBoxShowAll.Size = New System.Drawing.Size(271, 36)
+        Me.CheckBoxShowAll.Size = New System.Drawing.Size(138, 19)
         Me.CheckBoxShowAll.TabIndex = 0
         Me.CheckBoxShowAll.Text = "Show all publications"
         Me.CheckBoxShowAll.UseVisualStyleBackColor = True
@@ -198,17 +210,17 @@ Partial Class Form1
         'Form1
         '
         Me.AllowDrop = True
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(787, 880)
+        Me.ClientSize = New System.Drawing.Size(424, 416)
         Me.Controls.Add(Me.GroupBoxAdvanced)
         Me.Controls.Add(Me.GroupBoxPub)
         Me.Controls.Add(Me.GroupBoxMedia)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.KeyPreview = True
-        Me.Margin = New System.Windows.Forms.Padding(6)
         Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(440, 1000)
+        Me.MinimumSize = New System.Drawing.Size(440, 360)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "JWLibrary-MediaInserter"
